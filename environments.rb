2f3976cc109b49eca5c9f722493778c00001f45f
shelling <navyblueshellingford@gmail.com>
2013-08-10 17:59:47 +0800

    add skeleton of four kinds of query APIs

--- environments.rb
+++ environments.rb
@@ -2,4 +2,23 @@ require "bundler/setup"
 Bundler.require :default
 
 class App < Sinatra::Base
+
+    get "/serial/:serial" do # 統一編號
+    end
+
+    post "/serial/:serial" do
+    end
+
+    put "/serial/:serial" do
+    end
+
+    get "/name/:name" do # 公司名稱
+    end
+
+    get "/business/:business" do # 所營事業項目
+    end
+
+    get "/center/:center/radius/:radius" do # 中心點 + 半徑
+    end
+
 end
