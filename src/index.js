3e97c0fb863a564531ad894bfe9e11a7e92c05ae
howardchi <howardchi@fougood.com>
2013-08-14 22:34:38 +0800

    seperate locate view, member view

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ require(["config"], function(config) {
   	'backbone',
     'ajax/ly_8',
     'ajax/tisa_person',
+    'view/map_view',
     'view/ly_view',
     'view/index_view',
   	
-  	], function($, _, Backbone, ajaxLy, ajaxTisa, LyView, IndexView){
+  	], function($, _, Backbone, ajaxLy, ajaxTisa, MapView, LyView, IndexView){
       'use strict';
       
       // render index out view
+      var mapView = new MapView();
       var lyView = new LyView();
   		var indexView = new IndexView();
   	}
---
+++ src/view/member_view.js
@@ -0,0 +1,8 @@
+define([
+
+	'jquery',
+	'underscore',
+	'backbone',
+	'firebase',
+	'avatars',
+	])
