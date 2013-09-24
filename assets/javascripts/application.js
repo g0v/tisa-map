20dfa7b65c4b71378f2cde185022d910b9df5bd7
shelling <navyblueshellingford@gmail.com>
2013-09-24 20:33:47 +0800

    remove lawmaker list, region of election of lawmakers, authentication and login functionality change the strategy

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,7 +1,3 @@
 //= require_tree .
 $(document).ready(function() {
-    $("#scroll-down").click(function(e) {
-        e.preventDefault();
-        $("html, body").animate({ scrollTop: $(window).height() }, 1000);
-    });
 });
--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 require(["config"], function(config) {
-
-  require([
-  	'jquery',
-  	'underscore',
-  	'backbone',
-    'ajax/ly_8',
-    'ajax/tisa_person',
-    'view/map_view',
-    'view/company_view',
-    'view/auth_view',
-    'view/ly_view',
-    '/assets/application.js',
-    'scrollTop'
-  ], function($, _, Backbone, ajaxLy, ajaxTisa, MapView, CompanyView, AuthView, LyView){
-      'use strict';
-      
-      // render index out view
-      var mapView = new MapView();
-      var companyView = new CompanyView();
-      //var authView = new AuthView();
-      var lyView = new LyView();
-  	}
-  )
-
+require([
+'jquery',
+'underscore',
+'backbone',
+'view/map_view',
+'view/company_view',
+'view/auth_view',
+'/assets/application.js',
+], function($, _, Backbone, MapView, CompanyView, AuthView){
+    'use strict';
+    var mapView = new MapView();
+    var companyView = new CompanyView();
+})
 })
