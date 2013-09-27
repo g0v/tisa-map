d859526b448c3ba5ee99b5eeaeba1d271366c470
shelling <navyblueshellingford@gmail.com>
2013-09-27 19:33:30 +0800

    merge LocateCollection/Model into MapView

--- src/view/map_view.js
+++ src/view/map_view.js
@@ -3,13 +3,47 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'collection/locate_collection',
 	'view/company_view',
 	'geosearch',
 	'geosearch_provider',
 	'leaflet',
 
-	], function($, _, Backbone, LocateCollection, CompanyView) {
+	], function($, _, Backbone, CompanyView) {
+
+                var LocateModel = Backbone.Model.extend({
+                    defaults: {
+                        latlng: null
+                    }
+                });
+                var LocateCollection = Backbone.Collection.extend({
+                    model: LocateModel,
+                    startLocate: function() {
+                        if (navigator.geolocation) {
+                            var that = this;
+                            navigator.geolocation.getCurrentPosition(
+                                function(option) {
+                                    that.add({latlng: option});
+                                },
+                                function(error) {
+                                    switch (error.code) {
+                                        case error.TIMEOUT:
+                                            alert('GPS 定位連線逾時，請手動輸入所在地');
+                                            break;
+                                        case error.POSITION_UNAVAILABLE:
+                                            alert('無法取得 GPS 定位，請手動輸入所在地');
+                                            break;
+                                        case error.PERMISSION_DENIED://拒絕
+                                            alert('不允許 GPS 自動定位，請手動輸入所在地');
+                                            break;
+                                        case error.UNKNOWN_ERROR:
+                                            alert('不明的錯誤，請手動輸入所在地');
+                                            break;
+                                    }
+                                }
+                            );
+                        }
+                    }
+                });
 
 		var mapView = Backbone.View.extend({
 
