c13ac5e2de41dcdcba88eac396f27552b9e8e067
shelling <navyblueshellingford@gmail.com>
2013-09-27 15:17:43 +0800

    now no members to mark

--- src/view/map_view.js
+++ src/view/map_view.js
@@ -50,23 +50,6 @@ define([
 				L.marker(setplace).addTo(this.map).bindPopup("<b>你現在在這！</b>").openPopup();
 				this._company_view.addlocateCenter({center: {lat: option.coords.latitude, lng: option.coords.longitude}})
 			},
-
-			markMember: function () {
-				var user = this.members.pop().attributes.user;
-
-				if(!user.latlng) {
-					user.latlng = []
-				}
-
-				lat_val  = user.latlng[0] || 24;
-				lng_val  = user.latlng[1] || 121;
-				var marker = L.marker(L.latLng(lat_val, lng_val), { title: user.avatar });
-				// add popup
-				marker.bindPopup('<img src="' + user.avatar + '" width="50"><br>' + user.displayName);
-				// add new layer to map
-				this.markers.addLayer(marker);
-				this.map.addLayer(this.markers);
-			}
 		});
 
 		return mapView;	
