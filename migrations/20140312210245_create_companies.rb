ab0af305f0f89ae0ae7e4e3f86668fb1321022a1
shelling <shelling@listia.com>
2014-03-16 14:38:38 +0800

    More descriptions on columns. Drop table when rollback.

--- migrations/20140312210245_create_companies.rb
+++ migrations/20140312210245_create_companies.rb
@@ -3,16 +3,17 @@ Sequel.migration do
   up do
     create_table :companies do
       primary_key :id
-    # column      :location,    geometry(Point, 4326)
-      column      :name,        String,     null: false
-      column      :taxid,       String,     null: false
-      column      :address,     String,     null: false
+      column      :location,    "geometry(Point, 4326)"
+      column      :taxid,       String,     null: false,    size: 8,      fixed: true
+      column      :name,        String,     null: false,    size: 128
+      column      :address,     String,     null: false,    text: true
       column      :categoies,   "text[]",   null: false
-      column      :status,      String,     null: false
-      column      :owner,       String,     null: false
+      column      :status,      String,     null: false,    size: 128
+      column      :owner,       String,     null: false,    size: 128
     end
   end
 
   down do
+    drop_table :companies
   end
 end
