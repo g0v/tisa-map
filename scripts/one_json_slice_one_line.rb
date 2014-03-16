21f5e32c1917f87d3af474b99f516aedc20138a9
shelling <shelling@listia.com>
2014-03-16 17:03:15 +0800

    Let the raw data importer align the new schema.
    - Categories should be text[]

--- scripts/one_json_slice_one_line.rb
+++ scripts/one_json_slice_one_line.rb
@@ -7,7 +7,23 @@ require "active_support/core_ext/hash/slice"
 File.open(ARGV[0], "r").each_with_index do |line, index|
     line.sub!(/^(\d\d\d\d\d\d\d\d),/, "")
     number = $1
-    line.sub!(/^{/, "{\"統編\": \"#{number}\" ,")
-    json = Oj.load(line)
-    puts Oj.dump(json.slice("統編", "公司名稱", "公司狀況", "公司所在地", "所營事業資料")) if json["公司名稱"]
+    json = Oj.load(line).merge("taxid" => number)
+
+    json["name"]      = json["公司名稱"]
+    json["address"]   = json["公司所在地"]
+    json["categoies"] = if json["所營事業資料"].is_a?(Array)
+                          json["所營事業資料"].map { |item|
+                            if item.is_a?(Array)
+                              item[1]
+                            elsif item.is_a?(String)
+                              item
+                            end
+                          }
+                        else
+                          []
+                        end
+    json["status"]    = json["公司狀況"]
+    json["owner"]     = json["代表人姓名"]
+
+    puts Oj.dump(json.slice("taxid", "name", "status", "address", "categoies")) if json["name"]
 end
