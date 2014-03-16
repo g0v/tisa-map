5d3416e7189e4704f246ee48cc0ab68ee961c62f
shelling <shelling@listia.com>
2014-03-16 21:42:48 +0800

    More precise description on table companies
    - Add unique constraint and index on taxid
    - Allow NULL value for most columns

--- migrations/20140312210245_create_companies.rb
+++ migrations/20140312210245_create_companies.rb
@@ -4,12 +4,12 @@ Sequel.migration do
     create_table :companies do
       primary_key :id
       column      :location,    "geometry(Point, 4326)"
-      column      :taxid,       String,     null: false,    size: 8,      fixed: true
-      column      :name,        String,     null: false,    size: 128
-      column      :address,     String,     null: false,    text: true
+      column      :taxid,       String,     null: false,    unique: true,   size: 8,    fixed: true,    index: true
+      column      :name,        String,     size: 128
+      column      :address,     String,     text: true
       column      :categoies,   "text[]",   null: false
-      column      :status,      String,     null: false,    size: 128
-      column      :owner,       String,     null: false,    size: 128
+      column      :status,      String,     size: 128
+      column      :owner,       String,     size: 128
     end
   end
 
