ee35ce75b66420cc3ef8c87d1c9c31282e76666a
MrOrz <johnsonliang7@gmail.com>
2014-03-29 21:49:23 +0800

    Remove /map

--- environments.rb
+++ environments.rb
@@ -119,11 +119,6 @@ class App < Sinatra::Base
         erb :'shenk/category', :locals => {rows: rows, activities: activities, reference: reference}
     end
 
-    get "/map" do
-        cache_control :no_cache, :max_age => 0
-        slim :'map', layout: :'layout/_layout'
-    end
-
     get "/ly" do
         slim :'ly', layout: :'layout/_layout'
     end
